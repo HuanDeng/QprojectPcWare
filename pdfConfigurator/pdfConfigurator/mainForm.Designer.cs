5f8102cfb2f145ea9e2e93e6cf58dc54c9480719
Huan Deng <huan_d@sina.com>
2016-05-19 23:55:58 +0800

    增加工程

---
+++ pdfConfigurator/pdfConfigurator/mainForm.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace pdfConfigurator
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainForm));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AlarmTypeCombBox = new System.Windows.Forms.ComboBox();
+            this.AlarmDelayCombBox = new System.Windows.Forms.ComboBox();
+            this.StartDelayCombBox = new System.Windows.Forms.ComboBox();
+            this.SamplingRateCombBox = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OriginalTimeZoneTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FirmwareVerTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialNumberTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LowAlarmATextBox = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.HighAlarmATextBOX = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.AlarmTypeCombBox);
+            this.groupBox1.Controls.Add(this.AlarmDelayCombBox);
+            this.groupBox1.Controls.Add(this.StartDelayCombBox);
+            this.groupBox1.Controls.Add(this.SamplingRateCombBox);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.OriginalTimeZoneTextBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.FirmwareVerTextBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.serialNumberTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(303, 222);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "System Setup";
+            // 
+            // AlarmTypeCombBox
+            // 
+            this.AlarmTypeCombBox.FormattingEnabled = true;
+            this.AlarmTypeCombBox.Items.AddRange(new object[] {
+            "Disable",
+            "Single",
+            "Cumulative"});
+            this.AlarmTypeCombBox.Location = new System.Drawing.Point(147, 187);
+            this.AlarmTypeCombBox.Name = "AlarmTypeCombBox";
+            this.AlarmTypeCombBox.Size = new System.Drawing.Size(150, 20);
+            this.AlarmTypeCombBox.TabIndex = 17;
+            // 
+            // AlarmDelayCombBox
+            // 
+            this.AlarmDelayCombBox.FormattingEnabled = true;
+            this.AlarmDelayCombBox.Items.AddRange(new object[] {
+            "0 minutes",
+            "5 minutes",
+            "30 minutes",
+            "45 minutes",
+            "60 minutes",
+            "90 minutes",
+            "120 minutes"});
+            this.AlarmDelayCombBox.Location = new System.Drawing.Point(147, 159);
+            this.AlarmDelayCombBox.Name = "AlarmDelayCombBox";
+            this.AlarmDelayCombBox.Size = new System.Drawing.Size(150, 20);
+            this.AlarmDelayCombBox.TabIndex = 16;
+            // 
+            // StartDelayCombBox
+            // 
+            this.StartDelayCombBox.FormattingEnabled = true;
+            this.StartDelayCombBox.Items.AddRange(new object[] {
+            "0 minutes",
+            "5 minutes",
+            "30 minutes",
+            "45 minutes",
+            "60 minutes",
+            "90 minutes",
+            "120 minutes"});
+            this.StartDelayCombBox.Location = new System.Drawing.Point(147, 133);
+            this.StartDelayCombBox.Name = "StartDelayCombBox";
+            this.StartDelayCombBox.Size = new System.Drawing.Size(150, 20);
+            this.StartDelayCombBox.TabIndex = 15;
+            // 
+            // SamplingRateCombBox
+            // 
+            this.SamplingRateCombBox.FormattingEnabled = true;
+            this.SamplingRateCombBox.Items.AddRange(new object[] {
+            "30 Seconds",
+            "5 minutes",
+            "10 minutes",
+            "30 minutes",
+            "60 minutes",
+            "90 minutes",
+            "120 minutes"});
+            this.SamplingRateCombBox.Location = new System.Drawing.Point(147, 106);
+            this.SamplingRateCombBox.Name = "SamplingRateCombBox";
+            this.SamplingRateCombBox.Size = new System.Drawing.Size(150, 20);
+            this.SamplingRateCombBox.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Alarm Type:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Alarm Delay:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Start Delay:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 109);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Sampling Rate:";
+            // 
+            // OriginalTimeZoneTextBox
+            // 
+            this.OriginalTimeZoneTextBox.Location = new System.Drawing.Point(147, 79);
+            this.OriginalTimeZoneTextBox.Name = "OriginalTimeZoneTextBox";
+            this.OriginalTimeZoneTextBox.ReadOnly = true;
+            this.OriginalTimeZoneTextBox.Size = new System.Drawing.Size(150, 21);
+            this.OriginalTimeZoneTextBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Original Time Zone:";
+            // 
+            // FirmwareVerTextBox
+            // 
+            this.FirmwareVerTextBox.Location = new System.Drawing.Point(147, 52);
+            this.FirmwareVerTextBox.Name = "FirmwareVerTextBox";
+            this.FirmwareVerTextBox.ReadOnly = true;
+            this.FirmwareVerTextBox.Size = new System.Drawing.Size(150, 21);
+            this.FirmwareVerTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Firmware Version:";
+            // 
+            // serialNumberTextBox
+            // 
+            this.serialNumberTextBox.Location = new System.Drawing.Point(147, 25);
+            this.serialNumberTextBox.Name = "serialNumberTextBox";
+            this.serialNumberTextBox.ReadOnly = true;
+            this.serialNumberTextBox.Size = new System.Drawing.Size(150, 21);
+            this.serialNumberTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Serial Number:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox2.Controls.Add(this.LowAlarmATextBox);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.HighAlarmATextBOX);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Location = new System.Drawing.Point(6, 240);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(303, 85);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Temperature Measurement";
+            // 
+            // LowAlarmATextBox
+            // 
+            this.LowAlarmATextBox.Location = new System.Drawing.Point(147, 49);
+            this.LowAlarmATextBox.Name = "LowAlarmATextBox";
+            this.LowAlarmATextBox.ReadOnly = true;
+            this.LowAlarmATextBox.Size = new System.Drawing.Size(150, 21);
+            this.LowAlarmATextBox.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 52);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 12);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "LLow Alarm(℃):";
+            // 
+            // HighAlarmATextBOX
+            // 
+            this.HighAlarmATextBOX.Location = new System.Drawing.Point(147, 22);
+            this.HighAlarmATextBOX.Name = "HighAlarmATextBOX";
+            this.HighAlarmATextBOX.ReadOnly = true;
+            this.HighAlarmATextBOX.Size = new System.Drawing.Size(150, 21);
+            this.HighAlarmATextBOX.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 25);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(95, 12);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "High Alarm(℃):";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.SaveButton.Location = new System.Drawing.Point(122, 340);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 3;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 370);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "mainForm";
+            this.Text = "DataLogger Configurator";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox serialNumberTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox OriginalTimeZoneTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox FirmwareVerTextBox;
+        private System.Windows.Forms.ComboBox AlarmTypeCombBox;
+        private System.Windows.Forms.ComboBox AlarmDelayCombBox;
+        private System.Windows.Forms.ComboBox StartDelayCombBox;
+        private System.Windows.Forms.ComboBox SamplingRateCombBox;
+        private System.Windows.Forms.TextBox LowAlarmATextBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox HighAlarmATextBOX;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button SaveButton;
+
+    }
+}
+
---
+++ pdfConfigurator/pdfConfigurator/mainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pdfConfigurator
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        string configFilePath="./Sys/Conf.cf";
+        pdfConfigParam PdfConfig; 
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            PdfConfig = new pdfConfigParam(configFilePath);
+        }
+
+    }
+    class pdfConfigParam 
+    {
+        char[] CompanyName=new char[20];
+        public string companyName;
+	    char[] Serialnumber=new char[7];
+        public string serialNumber;
+	    char[] ProductionDate=new char[32];
+        public string productionDate;
+	    char[] ProductLot=new char[32];
+        public string productLot;
+	    char[] FirmwareVersion=new char[32];
+        public string firmwareVersion;
+	    char[] OriginalTimeZone=new char[32];
+        public string originalTimeZone;
+        public UInt16 SamplingRate_s;
+        public UInt16 StartDelay_s;
+        public UInt16 AlarmDelay_s;
+	    char AlarmType;
+	    char ParameterCount;
+	    char[] ParamA_Name=new char[32];
+	    char[] ParamB_Name=new char[32];
+	    char[] ParamC_Name=new char[32];
+	    char[] ParamA_Unit=new char[4];
+	    char[] ParamB_Unit=new char[4];
+        char[] ParamC_Unit = new char[4];
+	    public float ParamA_HighAlarm;
+        public float ParamB_HighAlarm;
+        public float ParamC_HighAlarm;
+        public float ParamA_LowAlarm;
+        public float ParamB_LowAlarm;
+        public float ParamC_LowAlarm;
+        public pdfConfigParam(string filePath) 
+        {
+            try
+            {
+                byte[] paramBytes = File.ReadAllBytes(filePath);
+                int i = 0;
+                Array.Copy(paramBytes, i, CompanyName, 0, 20); i += 20;
+                Array.Copy(paramBytes, i, Serialnumber, 0, 7); i += 7;
+                Array.Copy(paramBytes, i, ProductionDate, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, ProductLot, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, FirmwareVersion, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, OriginalTimeZone, 0, 32); i += 32;
+                i++;
+
+                UInt16 templow = (UInt16)paramBytes[i++];
+                UInt16 tempHigh = (UInt16)paramBytes[i++];
+                tempHigh <<= 8;
+                SamplingRate_s = (UInt16)(tempHigh + templow);
+
+                templow = (UInt16)paramBytes[i++];
+                tempHigh = (UInt16)paramBytes[i++];
+                tempHigh <<= 8;
+                StartDelay_s = (UInt16)(tempHigh + templow);
+
+                templow = (UInt16)paramBytes[i++];
+                tempHigh = (UInt16)paramBytes[i++];
+                tempHigh <<= 8;
+                AlarmDelay_s = (UInt16)(tempHigh + templow);
+
+                AlarmType = (char)paramBytes[i++];
+                ParameterCount = (char)paramBytes[i++];
+                Array.Copy(paramBytes, i, ParamA_Name, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, ParamB_Name, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, ParamC_Name, 0, 32); i += 32;
+                Array.Copy(paramBytes, i, ParamA_Unit, 0, 4); i += 4;
+                Array.Copy(paramBytes, i, ParamB_Unit, 0, 4); i += 4;
+                Array.Copy(paramBytes, i, ParamC_Unit, 0, 4); i += 4;
+
+                ParamA_HighAlarm=BitConverter.ToSingle(paramBytes, i); i += 4;
+                ParamB_HighAlarm = BitConverter.ToSingle(paramBytes, i); i += 4;
+                ParamC_HighAlarm = BitConverter.ToSingle(paramBytes, i); i += 4;
+                ParamA_LowAlarm = BitConverter.ToSingle(paramBytes, i); i += 4;
+                ParamB_LowAlarm = BitConverter.ToSingle(paramBytes, i); i += 4;
+                ParamC_LowAlarm = BitConverter.ToSingle(paramBytes, i); i += 4;
+
+                //companyName = BitConverter.ToString(CompanyName);
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+
+        }
+
+    }
+}
